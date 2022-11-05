2ded2727ae66bf34e95367e474980f4616af9fa7
ajwlukas <ajwlukas@gmail.com>
2022-11-06 01:43:18 +0900

    Update ajwKeyBoard.h

--- ajwKeyBoard.h
+++ ajwKeyBoard.h
@@ -1,14 +1,7 @@
 #pragma once
 
-
-#define AJW_KEYBOARD_LIBNAME 
-
 #ifdef AJW_KEYBOARDDLL
-
 	#define AJW_KEYBOARD_DLLEXPORT __declspec(dllexport)
-
 #else
-
 	#define AJW_KEYBOARD_DLLEXPORT __declspec(dllimport)
-
 #endif
